ea5786c99aefb8382ddbe2770e6face2ed591257
AlHIO <alalthegamegun.c@nycu.edu.tw>
2024-09-29 02:37:30 +0800

    w2 ep6 init

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 from fastapi import FastAPI # type: ignore
-from enum import Enum
+from pydantic import BaseModel
+
+# from enum import Enum
 
 app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
 
 
 # http://localhost:8080
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/items/")
+async def read_items(q: str | None = None):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
 
 # # http://localhost:8080/items/{item_id}?q={q}
 
@@ -37,6 +52,7 @@ async def root():
 # async def read_file(file_path: str):
 #     return {"file_path": file_path}
 
-@app.get("/users/{user_id}/items/{item_id}")
-async def get_user(user_id: int, item_id: int, q1: int = 10, q2: int = 1):
-    return {"user_id": user_id,"item_id": item_id, "q1": q1, "q2": q2}
+# @app.get("/users/{user_id}/items/{item_id}")
+# async def get_user(user_id: int, item_id: int, q1: int = 10, q2: int = 1):
+#     return {"user_id": user_id,"item_id": item_id, "q1": q1, "q2": q2}
+
