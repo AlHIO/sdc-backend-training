1dfdd3ceabd07bc675a42514038795e536f5ed41
AlHIO <alalthegamegun.c@nycu.edu.tw>
2024-09-29 01:58:51 +0800

    Enum

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI # type: ignore
+from enum import Enum
 
 app = FastAPI()
 
@@ -8,16 +9,26 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-# http://localhost:8080/items/{item_id}?q={q}
+# # http://localhost:8080/items/{item_id}?q={q}
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: int, q: str = None):
+#     return {"item_id": item_id, "q": q}
 
 # http://localhost:8080/users
 
+class UserId(int, Enum):
+    Alice = 1
+    Bob = 2
+    Eve = 3
+
 @app.get("/users/{user_id}")
-async def get_users(user_id):
-    return{"user_id": user_id}
+async def get_users(user_id: UserId):
+    if user_id is UserId.Alice:
+        return {"user_id": user_id, "user_info": "Alice"}
+    if user_id.value == 2:
+        return {"user_id": user_id, "user_info": "Bob"}
+    
+    return{"user_id": user_id, "user_info": ""}
 
 # http://localhost:8080/users/{user_id}
