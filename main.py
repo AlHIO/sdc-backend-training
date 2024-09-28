40baba4e109728a2acd8e612e149429db4fe9ae8
AlHIO <alalthegamegun.c@nycu.edu.tw>
2024-09-29 01:48:05 +0800

    some change

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI # type: ignore
 
 app = FastAPI()
 
 
+# http://localhost:8080
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+# http://localhost:8080/items/{item_id}?q={q}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+
+# http://localhost:8080/items/{item_id}/reviews
