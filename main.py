f2a7e18a7335b7ddbcf04f738aa2f7039e9ad2ea
AlHIO <alalthegamegun.c@nycu.edu.tw>
2024-10-11 04:00:25 +0800

    HW3 already

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ app = FastAPI()
 # http://localhost:8080/docs
 
 
+class Item(BaseModel):
+    price: float
+    tax: float| None = None
+
+
 # 定義一個 Enum 來表示 sort_order 的合法值
 class SortOrder(str, Enum):
     asc = "asc"
@@ -16,7 +21,7 @@ class SortOrder(str, Enum):
 # GET /items/{item_id} Endpoint 
 @app.get("/items/{item_id}")
 async def read_items(
-    item_id: Annotated[int | None, Path(ge=1, le=1000,
+    item_id: Annotated[Decimal, Path(ge=1, le=1000,
                                         description="Item ID must be between 1 and 1000.")], 
     q: Annotated[str | None, Query(description = "Query 'q' must be between 3 and 50 characters.",
                                    min_length=3,
@@ -25,19 +30,20 @@ async def read_items(
 ):
     results = {"item_id": item_id}
     if q:
-        results.update({"description": "This is a sample item that matches the query {q}"})
+        results.update({"description": f"This is a sample item that matches the query {q}"})
     else:
-        "This is a sample item."
+        results.update({"description": "This is a sample item."})
+    
     if sort_order:
         results.update({"sort_order": sort_order})
     return results
 
 
 @app.put("/items/{item_id}")
-async def update_item(item_id: int, 
-                      item: Item, 
-                      q: str | None = None):
-    result = {"item_id": item_id, **item.dict()}
+async def update_item(item_id: Annotated[Decimal, Path(ge=1, le=1000, description="Item ID must be between 1 and 1000.")], 
+                      item: Item = None, 
+                      q: Annotated[str | None, Query(min_length=3, max_length=50, description="Query 'q' must be between 3 and 50 characters.")] = None):
+    result = {"item_id": item_id, **item.model_dump()}
     if q:
         result.update({"q": q})
     return result
